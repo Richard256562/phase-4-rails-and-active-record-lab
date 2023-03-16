78bc1e0e09695a057369d52a392a6d0d7c5bb22e
RichardOkongo <rirachard19@gmail.com>
2023-03-16 03:37:33 +0300

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create!(first_name: 'Richard', last_name: 'Okongo', grade: 90)
+Student.create!(first_name: 'Lionel', last_name: 'Messi', grade: 80)
+Student.create!(first_name: 'Luqman', last_name: 'Ruby', grade: 70)
